f40a20dc96a66097086bf45a89fa28f42a583a4c
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-05-08 20:17:26 +0200

    Update strlen and add test

--- include/algo/strlen.hpp
+++ include/algo/strlen.hpp
@@ -1,11 +1,37 @@
 #pragma once
 #include <stddef.h>
+#define NSC [[nodiscard]] static constexpr
+#define NX noexcept
 namespace algo
 {
-static constexpr size_t strlen(const char* s) noexcept
+NSC size_t strlen(const char* s) NX
 {
 	const char* needle = s;
 	while(*needle) ++needle;
 	return static_cast<size_t>(needle - s);
 }
+NSC size_t strlen(const char*const* arr, size_t extra_num) NX
+{
+	size_t length = 0;
+	do
+		length += strlen(*arr++);
+	while(extra_num--);
+	return length;
+}
+template<size_t N>
+NSC size_t strlen(const char*const (&arr)[N]) NX
+{
+	size_t length = 0;
+	for(const char* s : arr) length += strlen(s);
+	return length;
+}
+template<class Collection>
+NSC size_t strlen(const Collection& c) NX
+{
+	size_t length = 0;
+	for(const char* s : c) length += strlen(s);
+	return length;
+}
 } // namespace algo
+#undef NSC
+#undef NX
--- tests/algo/strlen.cpp
+++ tests/algo/strlen.cpp
@@ -1,4 +1,5 @@
 #include <algo/strlen.hpp>
+#include <thin/strar.hpp>
 
 extern "C" int puts(const char*) noexcept;
 
@@ -8,7 +9,12 @@ int main()
 
 	// Compile-time checks
 	constexpr const char* seven_chars {"1234567"};
+	constexpr const char* hello_world[] {"Hello", "world", "!"};
+	constexpr thin::strar string_array {"Super", "string", "array"};
 	static_assert(strlen(seven_chars) == 7);
+	static_assert(strlen(hello_world + 1, 1) == 6);
+	static_assert(strlen(hello_world) == 11);
+	static_assert(strlen(string_array) == 16);
 
 	puts("Success.");
 	return 0;
