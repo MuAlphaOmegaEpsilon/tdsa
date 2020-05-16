d294be98519d175dc583c79b8e2c2aab06082d5e
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-05-17 00:49:31 +0200

    Use macro headers instead of macros embedded in scope_guard.hpp

--- include/thin/scope_guard.hpp
+++ include/thin/scope_guard.hpp
@@ -5,9 +5,7 @@ template<class Lambda>
 struct scope_guard
 {
 	Lambda onScopeExit;
-	scope_guard(Lambda lambda) noexcept : onScopeExit(lambda) {}
-	~scope_guard() noexcept { onScopeExit(); }
+	constexpr scope_guard(Lambda lambda) noexcept : onScopeExit(lambda) {}
+	constexpr ~scope_guard() noexcept { onScopeExit(); }
 };
 } // namespace thin
-#define CONCATENATE(x, y) x##y
-#define SCOPE_EXIT(f) scope_guard CONCATENATE(scopeGuard, __LINE__) { f }
--- tests/thin/scope_guard.cpp
+++ tests/thin/scope_guard.cpp
@@ -1,10 +1,14 @@
-#include <thin/scope_guard.hpp> // for array
+#include <tdsa/macro/scope_exit.hpp>
+#include <thin/scope_guard.hpp>
 extern "C" int puts(const char*) noexcept;
 extern "C" void exit(int) noexcept;
 
 int main()
 {
 	using namespace thin;
-	SCOPE_EXIT([](){ puts("Success."); exit(0); });
+	SCOPE_EXIT([]() {
+		puts("Success.");
+		exit(0);
+	});
 	return 1;
 }
