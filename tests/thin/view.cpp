1554ece8b0316f8517bae247d651dbe264aaf591
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-05-17 12:24:49 +0200

    Update view test

--- tests/thin/view.cpp
+++ tests/thin/view.cpp
@@ -10,10 +10,12 @@ int main()
 
 	// Compile-time checks
 	constexpr view<const int> ground_view {ground_truth + 1, 2};
+	constexpr view<const int, char> charsized_view {ground_truth, 5};
 	static_assert(ground_view.back() == 3);
 	static_assert(*ground_view.begin() == 2);
 	static_assert(*ground_view.end() == 4);
 	static_assert(ground_view[0] == 2);
+	static_assert(sizeof(char) == sizeof(charsized_view.size));
 
 	puts("Success.");
 	return 0;
