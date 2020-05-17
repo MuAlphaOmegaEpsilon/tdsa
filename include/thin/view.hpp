b79f2ca8b456c120d483169fe9f92273e13f3fcb
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-05-17 11:58:43 +0200

    Update thin::view to enable user defined integer size type

--- include/thin/view.hpp
+++ include/thin/view.hpp
@@ -8,19 +8,19 @@
 
 namespace thin
 {
-template<class T>
+template<class T, class SIZE_T = size_t>
 struct view
 {
 	T* data;
-	size_t size;
+	SIZE_T size;
 
 	// Utilities
 	ND CX bool empty() CNX { return !size; }
 	// Accessors
 	ND CX T& back() NX { return data[size - 1]; }
-	ND CX T& operator[](size_t index) NX { return data[index]; }
+	ND CX T& operator[](SIZE_T index) NX { return data[index]; }
 	ND CX const T& back() CNX { return data[size - 1]; }
-	ND CX const T& operator[](size_t index) CNX { return data[index]; }
+	ND CX const T& operator[](SIZE_T index) CNX { return data[index]; }
 	// Conversions
 	ND CX operator T*() NX { return data; }
 	ND CX operator const T*() CNX { return data; }
@@ -30,8 +30,8 @@ struct view
 	ND CX const T* begin() CNX { return data; }
 	ND CX const T* end() CNX { return data + size; }
 };
-template<class T>
-view(T*, size_t) -> view<T>;
+template<class T, class SIZE_T = size_t>
+view(T*, SIZE_T) -> view<T, SIZE_T>;
 } // namespace thin
 
 #undef NX
