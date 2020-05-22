cfb981338a6774fc2c50d3b87b578642350ab441
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-05-23 00:55:23 +0200

    clang-format scope_exit.hpp

--- include/tdsa/macro/scope_exit.hpp
+++ include/tdsa/macro/scope_exit.hpp
@@ -1,4 +1,5 @@
 #pragma once
 #include "unique.hpp"
 #include <thin/scope_guard.hpp>
-#define SCOPE_EXIT(f) scope_guard UNIQUE(scopeGuard) { f }
+#define SCOPE_EXIT(f)                                                          \
+	thin::scope_guard UNIQUE(scopeGuard) { f }
