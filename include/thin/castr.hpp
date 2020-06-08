ddbc7fec98e385ffc38fc020a12e83267269baa6
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-06-09 00:32:53 +0200

    Add support for zero sized castr

--- include/thin/castr.hpp
+++ include/thin/castr.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "../algo/strlen.hpp"
+#include <assert.h>
 #include <stddef.h>
 
 #define NX noexcept
@@ -12,26 +13,55 @@ namespace thin
 template<size_t N>
 struct castr
 {
-	char data[N];
+	char data[N ? N : 1] {};
 
 	// Utilities
 	ND CX bool empty() CNX { return !length(); }
-	ND CX size_t length() CNX { return algo::strlen(data); }
+	ND CX size_t length() CNX
+	{
+		if constexpr(N < 2) return 0;
+		return algo::strlen(data);
+	}
 	ND CX size_t size() CNX { return N; }
 	// Accessors
-	ND CX char& back() NX { return data[N - 2]; }
-	ND CX char& operator[](size_t index) NX { return data[index]; }
-	ND CX const char& back() CNX { return data[N - 2]; }
-	ND CX const char& operator[](size_t index) CNX { return data[index]; }
+	ND CX char& back() NX
+	{
+		if constexpr(N < 2) return data[0];
+		return data[N - 2];
+	}
+	ND CX char& operator[](size_t index) NX
+	{
+		assert(index < N);
+		return data[index];
+	}
+	ND CX const char& back() CNX
+	{
+		if constexpr(N < 2) return data[0];
+		return data[N - 2];
+	}
+	ND CX const char& operator[](size_t index) CNX
+	{
+		assert(index < N);
+		return data[index];
+	}
 	// Conversions
 	ND CX operator char*() NX { return data; }
 	ND CX operator const char*() CNX { return data; }
 	// Iterators
 	ND CX char* begin() NX { return data; }
-	ND CX char* end() NX { return data + N - 1; }
+	ND CX char* end() NX
+	{
+		if constexpr(!N) return data;
+		return data + N - 1;
+	}
 	ND CX const char* begin() CNX { return data; }
-	ND CX const char* end() CNX { return data + N - 1; }
+	ND CX const char* end() CNX
+	{
+		if constexpr(!N) return data;
+		return data + N - 1;
+	}
 };
+castr()->castr<0>;
 template<size_t N>
 castr(const char (&)[N]) -> castr<N>;
 } // namespace thin
