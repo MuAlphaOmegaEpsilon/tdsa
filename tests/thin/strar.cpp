4a7498faa077c7de30905d962209cc5b8b99c2be
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-06-09 20:04:09 +0200

    Add more tests to strar

--- tests/thin/strar.cpp
+++ tests/thin/strar.cpp
@@ -7,7 +7,12 @@ int main()
 	using namespace thin;
 
 	// Compile-time checks
+	constexpr strar<1> unused;
+	constexpr strar<5> one_to_five {"1", "2", "3", "4", "5"};
 	constexpr strar hello_world {"Hello", "C++", "world!"};
+	static_assert(unused[0] == nullptr);
+	static_assert(unused[0].length == 0);
+	static_assert(one_to_five[0][0] == '1');
 	static_assert((*hello_world.begin())[0] == 'H');
 	static_assert(*(hello_world.end() - 1)[0] == 'w');
 	static_assert(hello_world.size() == 3);
