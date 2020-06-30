34266212282b30b41d71a346ab28712cd70be52c
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-06-30 22:59:28 +0200

    Remove SCOPE_EXIT_VOID macro

--- include/tdsa/macro/scope_exit.hpp
+++ include/tdsa/macro/scope_exit.hpp
@@ -2,4 +2,3 @@
 #include "unique.hpp"
 #include <thin/scope_guard.hpp>
 #define SCOPE_EXIT thin::scope_guard UNIQUE(scopeGuard) =
-#define SCOPE_EXIT_VOID thin::scope_guard UNIQUE(scopeGuard) = []()
